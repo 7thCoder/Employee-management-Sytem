bb799e79d35399840a856fc6a04699d81241f539
7thCoder <nuibima@gmail.com>
2023-08-23 14:50:10 +0100

    "added and updated project files"

--- EmployeeSystem/EmployeeSystem/src/main/java/com/example/EmployeeSystem/Entity/Employee.java
+++ EmployeeSystem/EmployeeSystem/src/main/java/com/example/EmployeeSystem/Entity/Employee.java
@@ -1,9 +1,27 @@
 package com.example.EmployeeSystem.Entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
-
+@Table(name="tbl_employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String department; 
+
 }
